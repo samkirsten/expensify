ac028db049c6f6d0b1bfa48a297bdaf01566472a
Sam <sam.kirsten@gmail.com>
2018-06-01 12:59:21 +0100

    modified footer

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: '£xpen$ify',
+  serviceName: 'Expenses',
 
   // Default port that prototype runs on
   port: '3000',
